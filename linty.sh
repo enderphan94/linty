c26354d6b9dcdbfb2e79ee883659e5a05022e794
enderphan <lphanvan94@gmail.com>
2021-09-13 11:54:47 +0700

    added uint check

--- linty.sh
+++ linty.sh
@@ -20,3 +20,12 @@ echo
 echo "======TYPOS Check======"
 echo
 cspell-cli $file
+echo
+echo "======uint Check======"
+echo
+count=$(grep -wn "uint" $file | wc -l|tr -d ' ')
+if [ -n "$count" ]; then
+	echo "$count uint place(s) have been found"
+else
+	echo "No uint found"
+fi
