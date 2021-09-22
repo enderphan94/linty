0d900dcba6cb49b3c3c6dce739332208c357c550
enderphan <lphanvan94@gmail.com>
2021-09-22 21:57:00 +0700

    deleted myDirectoryName from git

--- node_modules/@remix-project/remix-analyzer/solidity-analyzer/modules/categories.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    SECURITY: { displayName: 'Security', id: 'SEC' },
-    GAS: { displayName: 'Gas & Economy', id: 'GAS' },
-    MISC: { displayName: 'Miscellaneous', id: 'MISC' },
-    ERC: { displayName: 'ERC', id: 'ERC' }
-};
-//# sourceMappingURL=categories.js.map
